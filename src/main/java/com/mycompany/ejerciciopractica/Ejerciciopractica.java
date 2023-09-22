baece9accfed845fce2f3f905de4a63b55a9d06b
noravazquez <vazquez.nora.g@gmail.com>
2023-09-21 19:33:41 -0600

    primer commit

---
+++ src/main/java/com/mycompany/ejerciciopractica/Ejerciciopractica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciopractica;
+
+/**
+ *
+ * @author Vazqu
+ */
+public class Ejerciciopractica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
